8a18575af3601a06a5c82aedb411c9a6f6d6c66d
Stephen Green <stephengreen1028@gmail.com>
2020-02-10 20:33:56 -0700

    Created assets folder with style.css and brought in the loading gif, also finished app.js logic to enhance the UX by hiding the loader after two seconds, displaying results after two seconds, and hiding both if there's an error.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculatedResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
+
+  // Show Loader
+  document.getElementById('loading').style.display = 'block';
+
+  // Show Results after two seconds
+  setTimeout(calculatedResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculatedResults(e) {
+function calculatedResults() {
 
   // UI Variables
   const amount = document.getElementById('amount');
@@ -18,35 +29,53 @@ function calculatedResults(e) {
   // Compute Monthly Payment 
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal * x * calculatedInterest) / (x - 1);
+
   // Check to see if the number is finite
   if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2); // <-- if finite, captures and displays value
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // Show Results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
     showError('Please check your numbers');
   }
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide Loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div'); // <-- dynamically creates a div
+
   // Get Elements
   const card = document.querySelector('.card'); // <-- selects the card class from the DOM
   const heading = document.querySelector('.heading'); // <-- selects the heading class from the DOM
+
   // Add Class
   errorDiv.className = 'alert alert-danger'; // <-- creates an alert with red text and body
+
   // Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error)); // <-- takes the newly created errorDiv and inserts a text node
+
   // Insert error above heading
-  card.insertBefore(errorDiv, heading);
+  card.insertBefore(errorDiv, heading); // <-- call a parent (line 37) and put in the element you want to insert (errorDiv) and then put in whatever you want to insert it before (heading)
+
   // Clear error after 3 seconds
   setTimeout(clearError, 3000);
 }
 
 // Clear Error
 function clearError() {
-  document.querySelector('.alert').remove();
+  document.querySelector('.alert').remove(); // <-- grabs the alert class and removes it after three seconds
 }
